9a714b4b69af163d44c6c604a10570aa121b0b53
neyzar1111 <choka95@hotmail.com>
2021-10-18 12:07:27 +0200

    Add refreshToken function that updates lS values

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -18,6 +18,37 @@ const LOCALSTORAGE_VALUES = {
 
 
 
+
+/**
+ * Use the refresh token in localStorage to hit the /refresh_token endpoint
+ * in our Node app, then update values in localStorage with data from
+ * **/
+const refreshToken = async () => {
+    try {
+        // Logout if there's no refresh token stored or we've managed to get into a reload infinite loop
+        if (!LOCALSTORAGE_VALUES.refreshToken ||
+            LOCALSTORAGE_VALUES.refreshToken === 'undefined' ||
+            (Date.now() - Number(LOCALSTORAGE_VALUES.timestamp) / 1000) < 1000
+        ) {
+            console.error('No refresh token available');
+            logout();
+        }
+
+        // Use `/refresh_token` endpoint from our Node app
+        const { data } = await axios.get(`/refresh_token?refresh_token=${LOCALSTORAGE_VALUES.refreshToken}`);
+
+        // Update localStorage values
+        window.localStorage.setItem(LOCALSTORAGE_KEYS.accessToken, data.access_token);
+        window.localStorage.setItem(LOCALSTORAGE_KEYS.timestamp, Date.now());
+
+        // Reload the page for localStorage updates to be reflected
+        window.location.reload();
+
+    } catch (e) {
+        console.error(e);
+    }
+};
+
 const getAccessToken = ()=>{
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
