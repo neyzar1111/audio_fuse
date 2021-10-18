53de5cb24768ff8e741d202e12ee3735f160cbf7
Anna Ikoki <ikokianna@gmail.com>
2021-10-18 15:08:03 +0200

    Add utility function

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const port = 8888;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
 // test on terminal
-console.log(process.env.CLIENT_ID);
+// console.log(process.env.CLIENT_ID);
 
 app.get('/', (req, res)=> {
   // res.send('hello World!');
@@ -19,13 +19,30 @@ app.get('/', (req, res)=> {
   res.json(data);
 });
 
+/**
+ * Generate a random string containing numbers and letters
+ * @param {number} length The length of the string
+ * @return {string} The generated string
+ */
+
+const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for(let i = 0; i < length; i++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+// obeserve terminal
+console.log(generateRandomString(16));
+
 app.get('/login', (req, res)=> {
+
 // redirect to the spotify account service url login page from http://localhost:8888/login
 res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
 });
-
-
-const port = 8888;
+// https://accounts.spotify.com/en/authorize?client_id=d88b20c9c8f04ffc9482309a3210bbcc&response_type=code&redirect_uri=http:%2F%2Flocalhost:8888%2Fcallback
 
 app.listen(port, ()=>{
   console.log(`express app listening at http://localhost:${port}`);
