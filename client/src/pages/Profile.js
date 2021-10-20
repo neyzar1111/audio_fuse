397e286a899554401aba0b926af40024f3e01d8a
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-10-20 11:59:26 +0200

    Add count of playlist in Profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -15,7 +15,6 @@ const Profile = () => {
 
             const userPlaylists  = await getCurrentUserPlaylists();
             setPlaylists(userPlaylists.data);
-            console.log(userPlaylists)
         };
 
         catchErrors(fetchData());
@@ -34,9 +33,12 @@ const Profile = () => {
                                 <div className="header__overline">Profile</div>
                                 <h1 className="header__name">{profile.display_name}</h1>
                                 <p className="header__meta">
-                  <span>
-                    {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ''}
-                  </span>
+                                    {playlists && (
+                                        <span>{playlists.total} Playlist{playlists.total !== 1 ? 's': ''}</span>
+                                    )}
+                                    <span>
+                                        {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ""}
+                                    </span>
                                 </p>
                             </div>
                         </div>
