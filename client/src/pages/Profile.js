4a2f270960457efd6c8ea1f3735d0f11bea64ee4
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-10-20 13:20:09 +0200

    Add Top artist section and styles for it

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
-import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists} from '../spotify';
-import { StyledHeader } from '../styles';
+import {StyledHeader} from "../styles";
+import {SectionWrapper, ArtistsGrid,} from "../components";
+
+
+import {
+    getCurrentUserProfile,
+    getCurrentUserPlaylists,
+    getTopArtists,
+} from '../spotify';
+
 
 const Profile = () => {
     const [profile, setProfile] = useState(null);
     const [playlists, setPlaylists] = useState(null);
     const [topArtists, setTopArtists] = useState(null);
 
-
     useEffect(() => {
+
         const fetchData = async () => {
             const userProfile  = await getCurrentUserProfile();
             setProfile(userProfile.data);
@@ -19,34 +27,47 @@ const Profile = () => {
 
             const userTopArtists  = await getTopArtists();
             setTopArtists(userTopArtists.data);
-        };
 
+
+
+        };
         catchErrors(fetchData());
     }, []);
+
 
     return (
         <>
             {profile && (
                 <>
-                    <StyledHeader type="user">
-                        <div className="header__inner">
-                            {profile.images.length && profile.images[0].url && (
-                                <img className="header__img" src={profile.images[0].url} alt="Avatar"/>
-                            )}
-                            <div>
-                                <div className="header__overline">Profile</div>
-                                <h1 className="header__name">{profile.display_name}</h1>
-                                <p className="header__meta">
-                                    {playlists && (
-                                        <span>{playlists.total} Playlist{playlists.total !== 1 ? 's': ''}</span>
-                                    )}
-                                    <span>
-                                        {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ""}
-                                    </span>
-                                </p>
+                    <div>
+
+                        <StyledHeader type="user">
+                            <div className="header__inner">
+                                {profile.images.length && profile.images[0].url && (
+                                    <img className="header__img" src={profile.images[0].url} alt="Avatar"/>
+                                )}
+                                <div>
+                                    <div className="header__overline">Profile</div>
+                                    <h1 className="header__name">{profile.display_name}</h1>
+                                    <p className="header__meta">
+                                        {playlists && (
+                                            <span>{playlists.total} Playlist{playlists.total !== 1 ? 's': ''}</span>
+                                        )}
+                                        <span>
+                                           {profile.followers.total} Follower{profile.followers.total !== 1 ? 's' : ""}
+                                       </span>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                    </StyledHeader>
+                        </StyledHeader>
+                        {topArtists && (
+                            <main>
+                                <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
+                                    <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+                                </SectionWrapper>
+                            </main>
+                        )}
+                    </div>
                 </>
             )}
         </>
