319f1490bd90b6f4cd9616e4a3f3fc14954d1c6d
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-10-20 16:28:02 +0200

    Add TopTracks page and styles to it

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import {
     useLocation
 } from "react-router-dom";
 import {GlobalStyle} from "./styles";
-import {Login,Profile,TopArtists} from "./pages";
+import {Login, Profile, TopArtists, TopTracks} from "./pages";
 import styled from "styled-components/macro";
 
 
@@ -69,7 +69,7 @@ function App() {
                            <TopArtists />
                         </Route>
                         <Route path="/top-tracks">
-                            <h1>Top Tracks</h1>
+                            <TopTracks />
                         </Route>
                         <Route path="/playlists/:id">
                             <h1>Playlist</h1>
---
+++ client/src/pages/TopTracks.js
@@ -0,0 +1,35 @@
+import {useEffect, useState} from "react";
+import { getTopTracks} from "../spotify";
+import {catchErrors} from "../utils";
+import {SectionWrapper, TrackList, TimeRangeButtons} from "../components";
+
+
+const TopTracks =()=> {
+    const [topTracks, setTopTracks] = useState(null);
+    const [activeRange, setActiveRange] = useState("short");
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const userTopTracks  = await getTopTracks(`${activeRange}_term`);
+            setTopTracks(userTopTracks.data);
+        };
+        catchErrors(fetchData());
+    }, [activeRange]);
+
+    return (
+        <>
+            <main>
+
+                {topTracks && (
+                    <SectionWrapper title="Top artists" breadcrumb="true">
+                        <TimeRangeButtons activeRange={activeRange} setActiveRange={setActiveRange} />
+                        <TrackList tracks={topTracks.items} />
+                    </SectionWrapper>
+                )}
+
+            </main>
+        </>
+    );
+}
+export default TopTracks;
