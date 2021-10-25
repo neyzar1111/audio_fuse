45a6e55319c490a66d6cd2194756170634a4e000
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-10-25 14:44:04 +0200

    Add sorting to a playlist

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -1,8 +1,8 @@
-import { useState, useEffect, useMemo} from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { catchErrors } from '../utils'
-import { getPlaylistById } from '../spotify';
+import { getPlaylistById, getAudioFeaturesForTracks } from '../spotify';
+import { catchErrors } from '../utils';
 import { TrackList, SectionWrapper } from '../components';
 import { StyledHeader } from '../styles';
 
@@ -11,6 +11,9 @@ const Playlist = () => {
     const [playlist, setPlaylist] = useState(null);
     const [tracksData, setTracksData] = useState(null);
     const [tracks, setTracks] = useState(null);
+    const [audioFeatures, setAudioFeatures] = useState(null);
+    const [sortValue, setSortValue] = useState(null);
+    const sortOptions = ['danceability', 'tempo', 'energy'];
 
     // console.log(playlist)
     // {
@@ -71,15 +74,62 @@ const Playlist = () => {
         ]));
 
         catchErrors(fetchMoreData());
+
+
+        //getting audio features
+        const fetchAudioFeatures = async () => {
+            const ids = tracksData.items.map(({ track }) => track.id).join(',');
+            const { data } = await getAudioFeaturesForTracks(ids);
+            setAudioFeatures(audioFeatures => ([
+                ...audioFeatures ? audioFeatures : [],
+                ...data['audio_features']
+            ]));
+        };
+        catchErrors(fetchAudioFeatures());
+
     }, [tracksData]);
 
-
-    const tracksForTracklist = useMemo(() => {
-        if (!tracks) {
-            return;
+    //Adding features to the tracks
+    const tracksWithAudioFeatures = useMemo(() => {
+        if (!tracks || !audioFeatures) {
+            return null;
         }
-        return tracks.map(({ track }) => track);
-    }, [tracks]);
+
+        return tracks.map(({ track }) => {
+            const trackToAdd = track;
+
+            if (!track.audio_features) {
+                const audioFeaturesObj = audioFeatures.find(item => {
+                    if (!item || !track) {
+                        return null;
+                    }
+                    return item.id === track.id;
+                });
+
+                trackToAdd['audio_features'] = audioFeaturesObj;
+            }
+
+            return trackToAdd;
+        });
+    }, [tracks, audioFeatures]);
+
+    // Sort tracks by audio feature to be used in template
+    const sortedTracks = useMemo(() => {
+        if (!tracksWithAudioFeatures) {
+            return null;
+        }
+
+        return [...tracksWithAudioFeatures].sort((a, b) => {
+            const aFeatures = a['audio_features'];
+            const bFeatures = b['audio_features'];
+
+            if (!aFeatures || !bFeatures) {
+                return false;
+            }
+
+            return bFeatures[sortValue] - aFeatures[sortValue];
+        });
+    }, [sortValue, tracksWithAudioFeatures]);
 
 
     return (
@@ -106,8 +156,28 @@ const Playlist = () => {
 
                     <main>
                         <SectionWrapper title="Playlist" breadcrumb={true}>
-                            {tracksForTracklist && (
-                                <TrackList tracks={tracksForTracklist} />
+                            <div>
+                                <label className="sr-only" htmlFor="order-select">
+                                    Sort tracks
+                                </label>
+                                <select
+                                    name="track-order"
+                                    id="order-select"
+                                    onChange={e=>setSortValue(e.target.value)}
+                                >
+                                    <option value=" ">Sort tracks</option>
+                                    {sortOptions.map((option,i)=>(
+                                        <option value={option} key={i}>
+                                            {
+                                                `${option.charAt(0).toUpperCase()}${option.slice(1)}`
+                                            }
+                                        </option>
+                                    ))}
+                                    
+                                </select>
+                            </div>
+                            {sortedTracks && (
+                                <TrackList tracks={sortedTracks} />
                             )}
                         </SectionWrapper>
                     </main>
@@ -118,126 +188,3 @@ const Playlist = () => {
 }
 
 export default Playlist;
-
-
-// import { useState, useEffect, useMemo } from 'react';
-// import axios from "axios";
-// import  { useParams } from 'react-router-dom';
-// import {catchErrors} from "../utils";
-// import {getPlaylistById, getAudioFeaturesForTracks} from "../spotify";
-// import {StyledHeader} from "../styles";
-// import {SectionWrapper, TrackList} from "../components";
-//
-// const Playlist =()=>{
-//     const {id} = useParams();
-//     const [playlist, setPlaylist] = useState(null);
-//     const [tracksData, setTracksData] = useState(null);
-//     const [tracks, setTracks] = useState(null);
-//     const [audioFeatures, setAudioFeatures ] =useState(null)
-//
-//     // get tracks data  gets 100 items
-//     useEffect(()=>{
-//         const fetchData = async () =>{
-//             const { data } = await getPlaylistById(id);
-//             setPlaylist(data); //getting general inf abt PL
-//             setTracksData(data.tracks); //getting tracks only
-//         };
-//         catchErrors(fetchData());
-//     },[id]);
-//
-//
-//     useEffect(()=>{
-//         if(!tracksData){
-//             return;
-//         }
-//         //When tracksData updates, check if there are more tracks to fetch
-//         //then update the state variable
-//         const fetchMoreData = async () => {
-//             if(tracksData.next){
-//                 const { data } = await axios.get(tracksData.next);
-//                 setTracksData(data);
-//             }
-//         }
-//         setTracks(tracks =>([
-//             ...tracks ? tracks : [],
-//             ...tracksData.items
-//         ]));
-//         catchErrors(fetchMoreData());
-//
-//         //Also update the audioFeatures state variable using the tracks IDs
-//         // we get audio features in array for every single song with id
-//         const fetchAudioFeatures = async () =>{
-//             const ids = tracksData.items.map( ({track})=> track.id ).join(",");
-//             const {data} = await getAudioFeaturesForTracks(ids);
-//             setAudioFeatures(audioFeatures =>([
-//                 ...audioFeatures ? audioFeatures : [],
-//                 ...data ['audio_features']
-//             ]))
-//         }
-//
-//         catchErrors(fetchAudioFeatures());
-//
-//     },[tracksData]);
-//     console.log(tracksData);
-//     console.log(audioFeatures);
-//
-//     const tracksWithAudioFeatures = useMemo(()=>{
-//         if(!tracks || !audioFeatures){
-//             return;
-//         }
-//
-//         return tracks.map(({track})=>{
-//             const trackToAdd = track;
-//             if(!track.audio_features){
-//                 const audioFeaturesObj = audioFeatures.find(function(item){
-//                     if(!item || !track){
-//                         return null;
-//                     }
-//                     return item.id === track.id;
-//                 });
-//                 trackToAdd['audio_features'] = audioFeaturesObj;
-//             }
-//             return trackToAdd;
-//         })
-//     },[tracks])
-//
-//
-//
-//     return(
-//         <>
-//             {playlist &&(
-//                 <>
-//                     <StyledHeader>
-//                         <div className="header__inner">
-//                             {playlist.images.length && playlist.images[0].url && (
-//                                 <img className="header__img"
-//                                      src={playlist.images[0].url}
-//                                      alt="Playlist Artwork"/>
-//                             )}
-//                             <div>
-//                                 <div className="header__overline" >Playlist</div>
-//                                 <h1 className="header__overline" >{playlist.name}</h1>
-//                                 <p className="header__meta">
-//                                     {playlist.followers.total ? (
-//                                         <span>{playlist.followers.total} {`follower ${playlist.followers.total !== 1 ? "s" : ""}`}</span>
-//                                     ): null}
-//                                     <span>{playlist.tracks.total}{ ` track${playlist.tracks.total !==1 ? "s" : ""}`}</span>
-//                                 </p>
-//                             </div>
-//                         </div>
-//                     </StyledHeader>
-//                     <main>
-//                         <SectionWrapper title='Playlist' breadcrumb={true}>
-//                             {tracksWithAudioFeatures && (
-//                                 <TrackList tracks={tracksWithAudioFeatures} />
-//                             )}
-//                         </SectionWrapper>
-//                     </main>
-//                 </>
-//             )}
-//
-//         </>
-//     )
-// };
-//
-// export default Playlist;
