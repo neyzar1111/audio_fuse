b1606273fe94a4ab3f75f06e32ea16360ced3372
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-11-02 09:11:06 +0100

    Add Podcasts

--- client/src/components/Audio.js
+++ client/src/components/Audio.js
@@ -1,10 +1,18 @@
 import React from "react";
 
-const Audio = ({book}) =>{
+const Audio = ({audio,i}) =>{
     return(
-            <div >
-                {book.title}
-            </div>
+            <>
+                    {audio && (
+                        <div className="audio__item">
+                            <div>
+                                <div className="audio__name">{audio.title}</div>
+                                <div className="audio__author">{audio.author}</div>
+                            </div>
+                            <div className="audio__description">{audio.description}</div>
+                        </div>
+                    )}
+            </>
     )
 }
 export default Audio;
--- client/src/pages/Podcasts.js
+++ client/src/pages/Podcasts.js
@@ -1,31 +1,51 @@
 import {useEffect, useState} from "react";
 import Data from "../Data";
-import {StyledRangeButtons} from "../styles"
+import {StyledAudioList} from "../styles";
+
 import {Audio, FilterButtons, SectionWrapper} from "../components";
 
 const Podcasts = () =>{
-    const [search, setSearch] = useState(null);
+    const [search, setSearch] = useState("");
+    const [found, setFound] = useState([]);
 
-    console.log(Data.books)
+    console.log(Data.books.length)
 
+    useEffect(()=>{
+        function getSearchedItems () {
+            setFound(()=>{
+                let filtered =  Data.books.filter(book=>{
+                    if(book.title.toLowerCase().indexOf(search.toLowerCase()) !== -1){
+                        return book;
+                    }
+
+                })
+                return filtered
+            })
+
+            console.log(found)
+        }
+
+        getSearchedItems()
+    },[search])
+    console.log("found", found)
     return (
         <>
             <div className="search__bar">
-                <input type="search"  placeholder="Search..." onChange={(e)=>setSearch(e.target.value)}/>
+                <input type="search"  placeholder="Search..." onChange={(e)=>(setSearch(e.target.value))}/>
             </div>
            <main>
                <SectionWrapper>
                     <FilterButtons />
                </SectionWrapper>
-               <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
-
-                   {Data.books &&(
-                       <>
-                           {Data.books.map((book) => (
-                               <Audio book={book} key={book.id}/>
+               <SectionWrapper title="" seeAllLink="">
+                   {found &&(
+                       <StyledAudioList>
+                           {found.map((book,i) => (
+                               <Audio audio={book} key={book.id} i={i}/>
                            ))}
-                       </>
-                    )}
+                       </StyledAudioList>
+                   )}
+
                </SectionWrapper>
 
            </main>
