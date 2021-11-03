e5756312535b10d80d2c51089f1ff9f3f08e5f64
Anna Ikoki <ikokianna@gmail.com>
2021-11-03 12:11:59 +0100

    Add random component

---
+++ client/src/pages/Random.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+import Data from "../Data";
+import {StyledAudioList} from "../styles";
+
+import {Audio, FilterButtons, SectionWrapper} from "../components";
+
+const Random = () =>{
+    
+      
+    return (
+        <>
+            <h1>Random songs go here</h1>
+        </>
+    );
+}
+export default Random;
