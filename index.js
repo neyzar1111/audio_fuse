40b160aa73de37d873f40e9a71e5191b46408269
Anna Ikoki <ikokianna@gmail.com>
2021-11-04 12:19:00 +0100

    Set config variables

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
 const axios = require('axios');
+const path = require('path');
 
 const app = express();
-const port = 8888;
+// const port = 8888;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8888;
 
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 /**
  * Generates a random string containing numbers and letters
@@ -82,7 +87,7 @@ app.get('/callback', (req, res) => {
             expires_in,
           });
 
-          res.redirect(`http://localhost:3000/?${queryParams}`);
+          res.redirect(`${FRONTEND_URI}?${queryParams}`);
 
         } else {
           res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
@@ -117,7 +122,11 @@ app.get('/refresh_token', (req, res) => {
       });
 });
 
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
-app.listen(port, () => {
-  console.log(`Express app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Express app listening at http://localhost:${PORT}`);
 });
